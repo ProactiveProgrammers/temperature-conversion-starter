b0c89ee32a34e829da8bfdb5a5b0883ba7c80b3d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-03-23 22:25:30 -0400

    feat: Add TODOs with hints and fix spacing in the converter/convert.py.

--- converter/converter/convert.py
+++ converter/converter/convert.py
@@ -1,5 +1,10 @@
 """Convert from Celsius to Fahrenheit and from Fahrenheit to Celsius."""
 
+# TODO: Add all of the required source code to this file so
+# that it can produce all of the required output
+
+# TODO: Add the required import to the top of this file for the units module
+
 from converter import units
 
 
@@ -16,17 +21,14 @@ def convert_fahrenheit_to_celsius(temperature: float) -> float:
     # TODO: delete this placeholder return statement and add the correct one
     return 0
 
+
 def convert_temperature(
     temperature: float,
     from_unit: units.TemperatureUnitOfMeasurement,
     to_unit: units.TemperatureUnitOfMeasurement,
 ):
     """Convert a temperature given the from_unit and the to_unit."""
-    converted_temperature = 0
-    # the requested temperature conversion is Celsius --> Fahrenheit
-    if from_unit.value == "Celsius" and to_unit.value == "Fahrenheit":
-        # TODO: call the function that will convert from Celsius to Fahrenheit
-    # the requested temperature conversion is Fahrenheit --> Celsius
-    elif from_unit.value == "Fahrenheit" and to_unit.value == "Celsius":
-        # TODO: call the function that will convert from Celsius to Fahrenheit
-    return converted_temperature
+    # TODO: the requested temperature conversion is Celsius --> Fahrenheit
+    # -- TODO: call the function that will convert from Celsius to Fahrenheit
+    # TODO: the requested temperature conversion is Fahrenheit --> Celsius
+    # -- TODO: call the function that will convert from Celsius to Fahrenheit
